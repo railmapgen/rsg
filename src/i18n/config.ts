590183d83b02a97eb9a1b0214ba8255cfc4823cd
xhcminecraft <xhcminecraft@163.com>
2025-09-14 21:41:48 +0800

    #2 fix part2

--- src/i18n/config.ts
+++ src/i18n/config.ts
@@ -8,7 +8,7 @@ import koTranslation from './translations/ko.json';
 
 const i18n = new rmgRuntime.I18nBuilder()
     .use(initReactI18next)
-    .withAppName('Sign Creater')
+    .withAppName('Rail Sign Generator')
     .withLng(rmgRuntime.getLanguage())
     .withResource('en', enTranslation)
     .withResource('zh-Hans', zhHansTranslation)
