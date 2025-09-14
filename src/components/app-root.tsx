f4605b6d83499bc4db7e4b5c2a8c85db5eb89209
xhcminecraft <162702963+xhcminecraft@users.noreply.github.com>
2025-09-14 18:36:21 +0800

    #3 fix

--- src/components/app-root.tsx
+++ src/components/app-root.tsx
@@ -1,17 +1,17 @@
-import rmgRuntime from '@railmapgen/rmg-runtime';
+//import rmgRuntime from '@railmapgen/rmg-runtime';
 import WindowHeader from './window-header';
-import { useRootDispatch, useRootSelector } from '../redux';
-import { bumpCounter } from '../redux/app/app-slice';
-import { useTranslation } from 'react-i18next';
-import { RMMantineProvider, RMPage, RMPageBody, RMPageHeader, RMWindow } from '@railmapgen/mantine-components';
-import { Button, Text, TextInput } from '@mantine/core';
+//import { useRootDispatch, useRootSelector } from '../redux';
+//import { bumpCounter } from '../redux/app/app-slice';
+//import { useTranslation } from 'react-i18next';
+import { RMMantineProvider, RMPage, RMPageBody, /*RMPageHeader,*/ RMWindow } from '@railmapgen/mantine-components';
+//import { Button, Text, TextInput } from '@mantine/core';
 import MetroSignGenerator from '../../scripts/signs';
-import React from 'react';
+//import React from 'react';
 
 export default function AppRoot() {
-    const { t } = useTranslation();
-    const dispatch = useRootDispatch();
-    const counter = useRootSelector(state => state.app.counter);
+    //const { t } = useTranslation();
+    //const dispatch = useRootDispatch();
+    //const counter = useRootSelector(state => state.app.counter);
 
     return (
         <RMMantineProvider>
@@ -20,9 +20,8 @@ export default function AppRoot() {
                 <RMPage>
                     <RMPageBody direction="column" px="xs">
                         {/* 直接渲染 MetroSignGenerator 组件 */}
-                        
+
                         <MetroSignGenerator />
-                        
                     </RMPageBody>
                 </RMPage>
             </RMWindow>
