73a424374d02d698fe992e89d7cd1ae2d2d6b133
xhcminecraft <xhcminecraft@163.com>
2025-09-20 16:07:15 +0800

    #11 fix  part1

--- src/components/window-header.tsx
+++ src/components/window-header.tsx
@@ -1,8 +1,10 @@
 import { useTranslation } from 'react-i18next';
 import rmgRuntime from '@railmapgen/rmg-runtime';
-import { MdOutlineHelpOutline } from 'react-icons/md';
+import { MdOutlineHelpOutline, MdTranslate } from 'react-icons/md';
 import { RMEnvBadge, RMWindowHeader } from '@railmapgen/mantine-components';
-import { ActionIcon, Title } from '@mantine/core';
+import { ActionIcon, Flex, Title } from '@mantine/core';
+import { IconButton, Menu, MenuButton, MenuItem, MenuList, HStack, flexbox } from '@chakra-ui/react';
+import { LANGUAGE_NAMES, LanguageCode } from '@railmapgen/rmg-translate';
 
 export default function WindowHeader() {
     const { t } = useTranslation();
@@ -10,14 +12,40 @@ export default function WindowHeader() {
     const environment = rmgRuntime.getEnv();
     const appVersion = rmgRuntime.getAppVersion();
 
+    const handleChangeLanguage = (language: LanguageCode) => {
+        rmgRuntime.getI18nInstance().changeLanguage(language);
+    };
+
     return (
         <RMWindowHeader>
             <Title>{t('head_bar.project_name')}</Title>
             <RMEnvBadge env={environment} ver={appVersion} />
-
-            <ActionIcon variant="subtle" color="gray" size="sm" aria-label={t('Help')} title={t('Help')} ml="auto">
-                <MdOutlineHelpOutline />
-            </ActionIcon>
+            <HStack overflowX="auto" ml={'auto'}>
+                <ActionIcon variant="subtle" color="gray" size="sm"></ActionIcon>
+                {rmgRuntime.isStandaloneWindow() && (
+                    <Menu>
+                        {/* 为MenuButton添加z-index确保按钮本身层级 */}
+                        <MenuButton
+                            as={IconButton}
+                            icon={<MdTranslate />}
+                            variant="ghost"
+                            size="sm"
+                            style={{ zIndex: 1000, borderWidth: 0 }}
+                        ></MenuButton>
+                        {/* 为MenuList添加更高的z-index确保菜单置顶 */}
+                        <MenuList>
+                            {(['en', 'zh-Hans', 'zh-Hant', 'ja', 'ko'] as LanguageCode[]).map(lang => (
+                                <MenuItem key={lang} onClick={() => handleChangeLanguage(lang)}>
+                                    {LANGUAGE_NAMES[lang][lang]}
+                                </MenuItem>
+                            ))}
+                        </MenuList>
+                    </Menu>
+                )}
+                <ActionIcon variant="subtle" color="gray" size="sm" aria-label={t('Help')} title={t('Help')} ml="auto">
+                    <MdOutlineHelpOutline />
+                </ActionIcon>
+            </HStack>
         </RMWindowHeader>
     );
 }
