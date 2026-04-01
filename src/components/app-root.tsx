4b820e1ef500c6b23a1500c40c74175d8db9266c
xhcminecraft <162702963+xhcminecraft@users.noreply.github.com>
2026-04-01 21:48:51 +0800

    #34 RSG2.1

--- src/components/app-root.tsx
+++ src/components/app-root.tsx
@@ -1,12 +1,21 @@
 //import rmgRuntime from '@railmapgen/rmg-runtime';
+
 import { ChakraProvider, createSystem, defaultConfig } from '@chakra-ui/react';
+
 import WindowHeader from './window-header';
+
 //import { useRootDispatch, useRootSelector } from '../redux';
+
 //import { bumpCounter } from '../redux/app/app-slice';
+
 //import { useTranslation } from 'react-i18next';
+
 import { RMMantineProvider, RMPage, RMPageBody, /*RMPageHeader,*/ RMWindow } from '@railmapgen/mantine-components';
+
 //import { Button, Text, TextInput } from '@mantine/core';
+
 import RailSignGenerator from './RailSignGenerator';
+
 //import React from 'react';
 
 const chakraSystem = createSystem(defaultConfig, {
@@ -15,7 +24,9 @@ const chakraSystem = createSystem(defaultConfig, {
 
 export default function AppRoot() {
     //const { t } = useTranslation();
+
     //const dispatch = useRootDispatch();
+
     //const counter = useRootSelector(state => state.app.counter);
 
     return (
@@ -23,6 +34,7 @@ export default function AppRoot() {
             <RMMantineProvider>
                 <RMWindow>
                     <WindowHeader />
+
                     <RMPage>
                         <RMPageBody direction="column" px="xs">
                             {/* 閻╁瓨甯村〒鍙夌厠 RailSignGenerator 缂佸嫪娆?*/}
